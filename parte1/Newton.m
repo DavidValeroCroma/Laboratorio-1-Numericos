d0c1b676e3af0f2f9bdec4b3f1cb469af4c19b4d
David Valero Croma <david.valero@usach.cl>
2022-09-20 02:22:57 -0400

    [David Valero]
    se completa la biseccion y newton para la parte 2 del enunciado

---
+++ parte1/Newton.m
@@ -0,0 +1,30 @@
+%definimos la funcion 
+f1 = @(x) x.^2 - 4 ; 
+%declaramos las expresiones correspondientes a la derivada de la funcion
+df1dx = @(x) 2*x;
+%asignamos el valor del X incial y pedimos los valores que competen a la
+%tolerancia buscada
+X0 = 4; %aqui para el informe hay que probar con diferentes entradas iniciales
+tol = input("Ingrese el valor de la tolerancia: ");
+maxIter = input("Ingrese la cantidad maxima de iteraciones: ");
+error = X0;
+iteraciones = 0;
+X = abs(X0);
+raiz = [X0];
+errores = [error];
+%procedemos con la iteracion
+while error > tol & iteraciones < maxIter
+    errorAux = error;
+    df = df1dx(X);
+    fx = f1(X);
+    %Se calcula el error en torno al valor anterior de x
+    X0 = X;
+    X = X - df\fx;
+    raiz = [raiz, X];
+    error = abs(X-X0); 
+    iteraciones = iteraciones + 1;
+    errores = [errores, error];
+
+end
+Y = log(errores);
+plot(errores, Y );
--- parte2/Secante.m
+++ parte2/Secante.m
@@ -1,7 +1,5 @@
 %definimos la funcion 
-f1 = @(x) x.^2 - 4 ; 
-%declaramos las expresiones correspondientes a la derivada de la funcion
-df1dx = @(x) 2*x;
+f1 = @(x) x*log(x) - cos(x);
 %asignamos el valor del X incial y pedimos los valores que competen a la
 %tolerancia buscada
 X0 = 1; %aqui para el informe hay que probar con diferentes entradas iniciales
@@ -9,18 +7,18 @@ tol = input("Ingrese el valor de la tolerancia: ");
 maxIter = input("Ingrese la cantidad maxima de iteraciones: ");
 error = X0;
 iteraciones = 0;
-X = X0;
+X = abs(X0);
 raiz = [X0];
 errores = [error];
 %procedemos con la iteracion
 while error > tol & iteraciones < maxIter
     df = df1dx(X);
-    fx = f1(X(1);
+    fx = f1(X);
     %Se calcula el error en torno al valor anterior de x
     X0 = X;
     X = X - df\fx;
     raiz = [raiz, X];
-    error = X-X0; 
+    error = abs(X-X0); 
     iteraciones = iteraciones + 1;
     errores = [errores, error];
 end
